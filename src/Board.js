37570705b63f3a9bf969f4fedc59bbcaff8a3840
jamaalcoder <kabarojamaal@gmail.com>
2023-05-15 01:53:19 +0100

    Done: ShipTivitas Step One

--- src/Board.js
+++ src/Board.js
@@ -75,4 +75,56 @@ export default class Board extends React.Component {
       </div>
     );
   }
+
+  componentDidMount(){
+    this.drake = Dragula([this.swimlanes.backlog.current,this.swimlanes.inProgress.current,this.swimlanes.complete.current]);
+    this.drake.on('drop',(el,target,source,sibling) => this.updateClient(el,target,source,sibling));
+  }
+  // clean listener
+  componentWillUnmount(){
+    this.drake.remove();
+  }
+
+  updateClient(el,target,_,sibling){
+    this.drake.cancel(true);
+
+    // ref of target swinlanes droped Card element
+    let targetSwimlane = "backlog";
+    if(target === this.swimlanes.inProgress.current){
+      targetSwimlane = "in-progress"
+    } else if(target === this.swimlanes.complete.current){
+      targetSwimlane = "complete"
+    }
+    
+    // create new array list
+    const clientList = [
+      ...this.state.clients.backlog,
+      ...this.state.clients.inProgress,
+      ...this.state.clients.complete,
+    ];
+    
+    const clientThatMoved = clientList.find(client => el && client.id === el.dataset.id);
+    const clientThatMovedClone = {
+      ...clientThatMoved,
+      status:targetSwimlane,
+    }
+
+    // remove the one element from orginal place return new list if moved
+    const updatedClient = clientList.filter(client => client.id !== clientThatMoved.id);
+
+    // get the index of the element sibling position dropped
+    const index = updatedClient.findIndex(client => sibling && client.id === sibling.dataset.id);
+
+    // update the list by order 
+    updatedClient.splice(index === -1 ? updatedClient.length : index , 0 , clientThatMovedClone);
+
+    // updated List state to effect the change in DOM
+    this.setState({
+      clients:{
+        backlog:updatedClient.filter(client => !client.status || client.status === "backlog"),
+        inProgress:updatedClient.filter(client => client.status && client.status === "in-progress"),
+        complete:updatedClient.filter(client => client.status && client.status === "complete"),
+      }
+    })
+  }
 }
